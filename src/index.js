7a4b702f9668ecf0034d0f0a3deb7e6a5058e624
Gabrijela Gavric <gabigavric@gmail.com>
2021-08-10 15:03:51 -0700

    checks if player has one and displayers winner

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ class Board extends React.Component {//parent class
 
   handleClick(i) {
     const squares = this.state.squares.slice(); //creates copy of squares to modify instead of modifying existing array
+    if(calculateWinner(squares) || squares[i]){//return early by ignoring a click if someone has won the game or if a Square is already filled:
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O'; //if value of xIsNext === true, then return 'X', else return 'O'
     this.setState({
       squares: squares,
@@ -35,7 +39,13 @@ class Board extends React.Component {//parent class
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares); //check if a player has won
+    let status;
+    if(winner){
+      status = 'Winner: ' + winner;
+    }else{
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' :'O');
+    }
 
     return (
       <div>
@@ -82,3 +92,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) { //given array of 9 squares,function will check for winner and return 'x', 'o', 'null'
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
